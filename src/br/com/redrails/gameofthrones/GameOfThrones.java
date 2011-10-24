8e3098d6d35e1684c0c275d7a73a9ddc5e088f5d
Luiz Carvalho <maximusmano@gmail.com>
2011-10-24 13:30:25 -0300

    atualizando para versão 2

--- src/br/com/redrails/gameofthrones/GameOfThrones.java
+++ src/br/com/redrails/gameofthrones/GameOfThrones.java
@@ -2,12 +2,10 @@ package br.com.redrails.gameofthrones;
 
 import java.io.IOException;
 
-
 import org.anddev.andengine.audio.music.Music;
 import org.anddev.andengine.audio.music.MusicFactory;
 import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.engine.camera.BoundCamera;
-import org.anddev.andengine.engine.handler.IUpdateHandler;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
@@ -17,8 +15,8 @@ import org.anddev.andengine.entity.layer.tiled.tmx.TMXLoader;
 import org.anddev.andengine.entity.layer.tiled.tmx.TMXTiledMap;
 import org.anddev.andengine.entity.layer.tiled.tmx.util.exception.TMXLoadException;
 import org.anddev.andengine.entity.modifier.IEntityModifier;
-import org.anddev.andengine.entity.modifier.PathModifier;
 import org.anddev.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
+import org.anddev.andengine.entity.modifier.PathModifier;
 import org.anddev.andengine.entity.modifier.PathModifier.IPathModifierListener;
 import org.anddev.andengine.entity.modifier.PathModifier.Path;
 import org.anddev.andengine.entity.scene.Scene;
@@ -31,9 +29,7 @@ import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.util.Debug;
 import org.anddev.andengine.util.modifier.IModifier;
 
-
 import android.util.Log;
-
 
 public class GameOfThrones extends BaseGame {
 	// ===========================================================
@@ -47,22 +43,20 @@ public class GameOfThrones extends BaseGame {
 	private static final int BACK = 1;
 	private static final int LEFT = 2;
 	private static final int RIGHT = 3;
-	private static final float BASE_SPEED = 1.2f;
+	private static final float BASE_SPEED = 0.6f;
 	private static final String TAG = "GAME OF THRONES";
-	
-
 
 	// ===========================================================
 	// Fields
 	// ===========================================================
 	private static AnimatedSprite player;
 	private static IEntityModifier mPathTemp;
-	private static boolean stoped=true;
 
-	static int mWaypointIndex=0;
+
 	private BoundCamera mBoundChaseCamera;
-	private int pathLeft = 6;
-	private int[][] circuit = {{300,600,BACK},{300,160,BACK},{10,160,LEFT},{580,160,RIGHT},{300,160,LEFT},{300,160,FRONT}};
+	private static int[] positions = { BACK,BACK, LEFT, BACK, RIGHT, FRONT, RIGHT, FRONT, FRONT};
+	// private int[][] circuit =
+	// {{300,600},{300,160,BACK},{10,160,LEFT},{580,160,RIGHT},{300,160,LEFT},{300,160,FRONT}};
 	private BitmapTextureAtlas mBitmapTextureAtlas;
 	private TiledTextureRegion mPlayerTextureRegion;
 	private TMXTiledMap mTMXTiledMap;
@@ -71,20 +65,27 @@ public class GameOfThrones extends BaseGame {
 
 	@Override
 	public Engine onLoadEngine() {
-		this.mBoundChaseCamera = new BoundCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mBoundChaseCamera).setNeedsMusic(true));
+		this.mBoundChaseCamera = new BoundCamera(0, 0, CAMERA_WIDTH,
+				CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+				new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT),
+				this.mBoundChaseCamera).setNeedsMusic(true));
 	}
 
 	@Override
 	public void onLoadResources() {
-		this.mBitmapTextureAtlas = new BitmapTextureAtlas(128, 128, TextureOptions.DEFAULT);
+		this.mBitmapTextureAtlas = new BitmapTextureAtlas(128, 128,
+				TextureOptions.DEFAULT);
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		MusicFactory.setAssetBasePath("mfx/");
-		this.mPlayerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "player.png", 0, 0, 3, 4); // 3x4 tiles and 72x128
-		
+		this.mPlayerTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createTiledFromAsset(this.mBitmapTextureAtlas, this,
+						"player.png", 0, 0, 3, 4); // 3x4 tiles and 72x128
+
 		try {
-            this.soundTrack = MusicFactory.createMusicFromAsset(this.mEngine.getMusicManager(), this, "track.ogg");
-            this.soundTrack.setLooping(true);
+			this.soundTrack = MusicFactory.createMusicFromAsset(
+					this.mEngine.getMusicManager(), this, "track.ogg");
+			this.soundTrack.setLooping(true);
 
 		} catch (final IOException e) {
 			Log.e("SOUND ERROR", e.getMessage());
@@ -95,12 +96,14 @@ public class GameOfThrones extends BaseGame {
 	@Override
 	public Scene onLoadScene() {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
-		//GameOfThrones.this.soundTrack.setLooping(true);
+		// GameOfThrones.this.soundTrack.setLooping(true);
 		GameOfThrones.this.soundTrack.play();
 		final Scene scene = new Scene();
 
 		try {
-			final TMXLoader tmxLoader = new TMXLoader(this, this.mEngine.getTextureManager(), TextureOptions.BILINEAR_PREMULTIPLYALPHA);			
+			final TMXLoader tmxLoader = new TMXLoader(this,
+					this.mEngine.getTextureManager(),
+					TextureOptions.BILINEAR_PREMULTIPLYALPHA);
 			this.mTMXTiledMap = tmxLoader.loadFromAsset(this, "tmx/castle.tmx");
 		} catch (final TMXLoadException tmxle) {
 			Debug.e(tmxle);
@@ -110,113 +113,119 @@ public class GameOfThrones extends BaseGame {
 		scene.attachChild(tmxLayer);
 
 		/* Make the camera not exceed the bounds of the TMXEntity. */
-		this.mBoundChaseCamera.setBounds(0, tmxLayer.getWidth(), 0, tmxLayer.getHeight());
+		this.mBoundChaseCamera.setBounds(0, tmxLayer.getWidth(), 0,
+				tmxLayer.getHeight());
 		this.mBoundChaseCamera.setBoundsEnabled(true);
 
-		/* Calculate the coordinates for the face, so its centered on the camera. */
-		final int centerX = (CAMERA_WIDTH - this.mPlayerTextureRegion.getTileWidth()) / 2;
-		final int centerY = (CAMERA_HEIGHT - this.mPlayerTextureRegion.getTileHeight()) / 2;
+		/*
+		 * Calculate the coordinates for the face, so its centered on the
+		 * camera.
+		 */
+		final int centerX = (CAMERA_WIDTH - this.mPlayerTextureRegion
+				.getTileWidth()) / 2;
+		final int centerY = (CAMERA_HEIGHT - this.mPlayerTextureRegion
+				.getTileHeight()) / 2;
 
 		/* Create the sprite and add it to the scene. */
-		player = new AnimatedSprite(centerX, centerY, this.mPlayerTextureRegion);
-		this.mBoundChaseCamera.setChaseEntity(player);//Cam follow o Player
-		player.setPosition(300,600);
-		playerAnimate(BACK);
-		walkToNextWayPoint(300, 160, scene);
+		player = new AnimatedSprite(300, 600, this.mPlayerTextureRegion);
 
+		this.mBoundChaseCamera.setChaseEntity(player);// Cam follow o Player
 		
-		scene.registerUpdateHandler(new IUpdateHandler() {
-			public void reset() { }
+		
+		walkToNextWayPoint(scene);
 
-			public void onUpdate(final float pSecondsElapsed) {
-				if(stoped){
-					playerAnimate(FRONT);
-					walkToNextWayPoint(300,600, scene);
-				}
-			}
-		});		
-		
-		
-		
 		scene.attachChild(player);
 		return scene;
 	}
+
 	@Override
 	public void onLoadComplete() {
-		
+
 	}
-	
 
-	private static void walkToNextWayPoint(final float pX, final float pY, final Scene pScene) {
-
-		
-		stoped=false;
+	private static void walkToNextWayPoint(final Scene pScene) {
 
 		// mPathTemp is another global PathModifier
-		player.unregisterEntityModifier(mPathTemp);
+		//player.unregisterEntityModifier(mPathTemp);
 
 		// create a new path with length 2 from current sprite position to next
 		// original path waypoint
-		final Path path = new Path(2);
-		path.to(player.getX(), player.getY()).to(pX,pY);
-		
+		final Path path = new Path(9);
+		path.to(player.getX(), player.getY()).to(300, 600).to(300, 180)
+				.to(70, 180).to(70, 90).to(260, 90).to(260, 122).to(290, 120).to(290, 130);
 
 		// recalculate the speed.TILE_WIDTH is the tmx tile width, use yours
-		//Adjust the speed for different control options
+		// Adjust the speed for different control options
 		float TileSpeed = path.getLength() * BASE_SPEED / (TILE_WIDTH);
-		Log.d(TAG,"SPEED: "+TileSpeed);
+		Log.d(TAG, "SPEED: " + TileSpeed);
 
 		// Create the modifier of this subpath
-		mPathTemp = new PathModifier(TileSpeed, path, new IEntityModifierListener() {
+		mPathTemp = new PathModifier(TileSpeed, path,
+				new IEntityModifierListener() {
 
-			public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
+					public void onModifierStarted(IModifier<IEntity> pModifier,
+							IEntity pItem) {
 
-			}
+					}
 
-			public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
+					public void onModifierFinished(
+							IModifier<IEntity> pModifier, IEntity pItem) {
 
-			}
-		}, new IPathModifierListener() {
+					}
+				}, new IPathModifierListener() {
 
-			public void onPathWaypointStarted(final PathModifier pPathModifier, final IEntity pEntity,
-					int pWaypointIndex) {				
+					public void onPathWaypointStarted(
+							final PathModifier pPathModifier,
+							final IEntity pEntity, int pWaypointIndex) {
+						Log.w(TAG,"On PWAYPonit Started");
+						playerAnimate(pWaypointIndex);
 
-			}
+					}
 
-			public void onPathWaypointFinished(PathModifier pPathModifier, IEntity pEntity, int pWaypointIndex) {
-				
-			}
+					public void onPathWaypointFinished(
+							PathModifier pPathModifier, IEntity pEntity,
+							int pWaypointIndex) {
+						Log.w(TAG,"On WayPonit Finished");
 
-			public void onPathStarted(PathModifier pPathModifier, IEntity pEntity) {
+					}
 
-			}
+					public void onPathStarted(PathModifier pPathModifier,
+							IEntity pEntity) {
+						Log.w(TAG,"On PATH STARTED");
 
-			public void onPathFinished(PathModifier pPathModifier, IEntity pEntity) {
-				player.stopAnimation();
-				stoped=true;				
-			}
-		});
+					}
+
+					public void onPathFinished(PathModifier pPathModifier,
+							IEntity pEntity) {
+						player.stopAnimation();
+						Log.w(TAG,"On PATH FINISHED");
+					}
+				});
 
 		player.registerEntityModifier(mPathTemp);
 	}
-	
-	private static void playerAnimate(int position){
-		switch(position) {
+
+	private static void playerAnimate(int position) {
+
+		switch (positions[position]) {
 		case FRONT:
-			player.animate(new long[]{200, 200, 200}, 6, 8, true);
+			player.animate(new long[] { 200, 200, 200 }, 0, 2, true);
+			Log.w(TAG,"Animate to FRONT");
 			break;
 		case RIGHT:
-			player.animate(new long[]{200, 200, 200}, 3, 5, true);
+			Log.w(TAG,"Animate to RIGHT");
+			player.animate(new long[] { 200, 200, 200 }, 6, 8, true);
 			break;
 		case BACK:
-			player.animate(new long[]{200, 200, 200}, 0, 2, true);
+			Log.w(TAG,"Animate to BACK");
+			player.animate(new long[] { 200, 200, 200 }, 9, 11, true);
 			break;
 		case LEFT:
-			player.animate(new long[]{200, 200, 200}, 9, 11, true);
+			Log.w(TAG,"Animate to LEFT");
+			player.animate(new long[] { 200, 200, 200 }, 3, 5, true);
 			break;
+		}
+
 	}
-		
-	}
-	
 
 }
